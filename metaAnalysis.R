078fe191fb0aeb257c67c9f1e88608bb26c7788c
Vaskoman <vassil.dimitrov@mail.mcgill.ca>
2018-09-21 12:40:11 -0400

    Initial code commit. Friday, Sep 21, 2018.

---
+++ metaAnalysis.R
@@ -0,0 +1,20 @@
+# Meta-analysis for top DO and GO-BP categories
+# Datasets and genes that belong to these categories
+# Novel biologically significant genes
+
+## FUNCTIONS ####
+
+## setwd, load libraries, & load data ####
+
+## DO categories with the most datasets ####
+# extract datasets belonging to each category
+# extract gene names belonging to each category
+# extract genes from master tables per category
+# go over to select novel relevant significant genes (just up but not down)
+
+
+## GO BP categories with the most datasets ####
+# extract datasets belonging to each category
+# extract gene names belonging to each category
+# extract genes from master tables per category
+# go over to select novel relevant significant genes (just up but not down)
